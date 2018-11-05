feedf9981a437ff2d2666db721e4d7a48941adca
heso6 <romaniuk99@gmail.com>
2018-11-05 10:23:58 +0100

    test

---
+++ src/ZipCode.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ZipCode {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Boolean adress = sc.hasNext("\\d{2}-\\d{3}");
+
+
+//        if (adress == true) {
+//            System.out.println("Podales poprawny adres");
+//        } else {
+//            System.out.println("Podales zly adres");
+//        }
+//        something
+    }
+}
+

b6ed5e13efe94047fa696ab71a957727ad81d79f
heso6 <romaniuk99@gmail.com>
2018-11-05 12:37:17 +0100

    Initial commit!

--- src/ZipCode.java
+++ src/ZipCode.java
@@ -7,7 +7,11 @@ public class ZipCode {
 
         Boolean adress = sc.hasNext("\\d{2}-\\d{3}");
 
-       
+       if (adress == true){
+           System.out.println("zip code is correct");
+       }else {
+           System.out.println("zip code is wrong");
+       }
        
     }
 }

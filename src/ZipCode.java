6698c176a5c0d98c62252070047f1ccb00248e18
heso6 <romaniuk99@gmail.com>
2018-11-06 12:18:21 +0100

    Update ZipCode.java

--- src/ZipCode.java
+++ src/ZipCode.java
@@ -6,12 +6,6 @@ public class ZipCode {
         Scanner sc = new Scanner(System.in);
 
         Boolean adress = sc.hasNext("\\d{2}-\\d{3}");
-
-        if (adress == true){
-            System.out.println("zip code is correct");
-        }else {
-            System.out.println("zip code is wrong!");
-        }
        
     }
 }
